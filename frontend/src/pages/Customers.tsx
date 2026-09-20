665ec2bd84faebf8ab40931dda35b16f2086104f
dulnith-liyanage <dnethmiraliyanage@gmail.com>
2026-09-20 20:01:41 +0530

    Fix syntax error in Customers.tsx feedback modal

--- frontend/src/pages/Customers.tsx
+++ frontend/src/pages/Customers.tsx
@@ -168,6 +168,7 @@ export default function Customers() {
                                       </div>
                                     </div>
                                   )}
+                                  </div>
                                 </div>
                               </DialogContent>
                             </Dialog>
